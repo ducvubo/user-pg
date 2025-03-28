41e767884236e01424bb5ef32e344a9c74a8075d
ducvubo <vminhduc8@gmail.com>
2025-03-28 14:04:33 +0700

    refactor: remove commented image code from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <>
       <HomePage />
-      {/* <Image src="/api/view-image?bucket=default&file=1741785668480-c6f2711a-bdab-4369-9e0e-bf27d207320f.webp" alt="food" width={1920} height={1080} /> */}
     </>
   )
 }
