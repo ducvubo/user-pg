3372729ce5e821238282fde72c2571d59980b34e
ducvubo <vminhduc8@gmail.com>
2025-04-11 07:46:36 +0700

    feat: add suspense fallback to page component for improved loading experience

--- src/app/mon-an-da-dat/page.tsx
+++ src/app/mon-an-da-dat/page.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import PageListOrderFood from './_component/PageListOrderFood'
 
 export default function page() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <PageListOrderFood />
-    </>
+    </Suspense>
   )
 }
