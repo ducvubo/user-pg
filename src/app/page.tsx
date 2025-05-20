12856f790a29e4e0465831590388a765333dc2a7
ducvubo <vminhduc8@gmail.com>
2025-05-20 08:34:57 +0700

    fix: Update server URL in environment file, add Google site verification, and refactor RestaurantDetail component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,42 @@
-import Image from 'next/image'
-import HomePage from './home/_component/Home'
-import { redirect } from 'next/navigation'
+// import Image from 'next/image'
+// import HomePage from './home/_component/Home'
+// import { redirect } from 'next/navigation'
 
-export default function Home() {
-  redirect('/nha-hang/Nha-PATO-390590.html')
+// export default function Home() {
+//   redirect('/nha-hang/Nha-PATO-390590.html')
+//   return (
+//     <>
+//       <HomePage />
+//     </>
+//   )
+// }
+
+import { IRestaurant } from '@/app/interface/restaurant.interface'
+import dynamic from 'next/dynamic'
+import { redirect } from 'next/navigation'
+import { getRestaurantBySlug } from './nha-hang/api'
+import InforRestaurant from './nha-hang/_component/InforRestaurant'
+const ToastServer = dynamic(() => import('@/components/ToastServer'), {
+  ssr: true
+})
+
+
+export default async function RestaurantDetail() {
+  const slug = 'Nha-PATO-390590.html'
+
+  const restaurant: IBackendRes<IRestaurant> = await getRestaurantBySlug(slug)
+
+  if (restaurant.statusCode !== 200 || !restaurant.data) {
+    return (
+      <div>
+        <ToastServer message='Không tìm thấy nhà hàng' title='Lỗi' variant='destructive' />
+      </div>
+    )
+  }
+
   return (
     <>
-      <HomePage />
+      <InforRestaurant slug={slug} restaurant={restaurant.data} />
     </>
   )
 }
